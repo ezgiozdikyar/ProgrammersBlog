8f4933a7a6af6bca0a03192e973890f6f738f36e
Ezgi Özdikyar <ezgiozdikyar@gmail.com>
2023-08-13 20:55:55 +0300

    Added services

---
+++ Blog/Services/Extensions/ServiceCollentionExtentions.cs
@@ -0,0 +1,20 @@
+﻿using Blog.DataAccess.Abstract;
+using Blog.DataAccess.Concrete;
+using Blog.Entities.Contexts;
+using Blog.Services.Abstract;
+using Blog.Services.Concrete;
+
+namespace Blog.Services.Extensions
+{
+    public static class ServiceCollentionExtentions
+    {
+        public static IServiceCollection LoadServices(this IServiceCollection services)
+        {
+            services.AddDbContext<BlogContext>();
+            services.AddScoped<IUnitOfWork, UnitOfWork>();
+            services.AddScoped<ICategoryService,CategoryManager>();
+            services.AddScoped<IArticleService,ArticleManager>();
+            return services;
+        }
+    }
+}
